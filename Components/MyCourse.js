e21197b5c21aa11ffbff7a77789eb4a400d61311
shadowwalker1025 <nagendra.tere@gmail.com>
2022-07-18 22:56:50 +0530

    First Commit

---
+++ Components/MyCourse.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MyCourse = () => {
+  return (
+    <View>
+      <Text>MyCourse</Text>
+    </View>
+  )
+}
+
+export default MyCourse
+
+const styles = StyleSheet.create({})
